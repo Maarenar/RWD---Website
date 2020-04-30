6649f028a74caeb9fee61ec786b2c38bf13e1707
Marion Renard <mrenard37@gmail.com>
2020-04-30 12:01:15 +0200

    retouches menu nav pour mobiles

--- controls.js
+++ controls.js
@@ -2,11 +2,7 @@
  * Open hamburger menu
  */
 
-function openMenu(){
-    var i = document.getElementById("menu_nav");
-    if(i.className === "menu_open"){
-       i.className = "menu_closed";
-    }else{
-        i.className = "menu_open";
-    }
+function closeMenu(){
+    var i = document.getElementById("menutoggle");
+    i.className = "menu_closed";
 }
